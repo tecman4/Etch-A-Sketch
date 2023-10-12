e88180506c28bfa1e64c3858163ec279e100fa79
Wayne <tecman4@gmail.com>
2023-10-12 15:00:31 -0400

    Fixed grid, added range slider & reset button

--- sketch.js
+++ sketch.js
@@ -1,22 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
 const p = document.querySelector('p');
-let pixels=32;
+let pixels=8;
 var root = document.querySelector(':root');
-root.style.setProperty('--pixels', '8');
+var nav = document.querySelector('nav');
+var slidecontainer = document.querySelector('.slidecontainer');
+var myRange = slidecontainer.querySelector('#myRange');
+var rangevalue = nav.querySelector('.rangevalue');
+var resetBoard = nav.querySelector('.resetBoard');
 
+resetBoard.addEventListener('click', function() {
+  p.innerHTML = '';
+  drawgrid();
+});
+
+slidecontainer.addEventListener('input', function() {
+root.style.setProperty('--pixels', myRange.value);
+pixels = myRange.value;
+p.innerHTML = '';
+drawgrid();
+rangevalue.textContent =  "Current value: "+ myRange.value + "x" + myRange.value;;
+
+});
+drawgrid();
+function drawgrid(){
 for(let i=0;i<pixels;i++){
   const column = document.createElement('column');
   column.textContent = '';
-  column.style.backgroundColor = getRandomColor();
   p.appendChild(column);
   column.addEventListener('mouseover', function() {
-    column.style.backgroundColor = getRandomColor();
   });
   for(let j=0;j<pixels;j++){
 
     const row = document.createElement('row');
     row.textContent = '';
-    row.style.backgroundColor = getRandomColor();
+    // row.style.backgroundColor = getRandomColor();
     column.appendChild(row);
     row.addEventListener('mouseover', function() {
       row.style.backgroundColor = getRandomColor();
@@ -24,7 +41,7 @@ for(let i=0;i<pixels;i++){
     
   }
 }
-
+}
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
