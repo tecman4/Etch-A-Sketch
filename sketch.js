0d02a7a57d8dbb03c5bb2cff5c3c8672fa8d867f
Wayne <tecman4@gmail.com>
2023-10-11 22:47:31 -0400

    Working on Grid

--- sketch.js
+++ sketch.js
@@ -5,14 +5,21 @@ var root = document.querySelector(':root');
 root.style.setProperty('--pixels', '8');
 
 for(let i=0;i<pixels;i++){
+  const column = document.createElement('column');
+  column.textContent = '';
+  column.style.backgroundColor = getRandomColor();
+  body.appendChild(column);
+  column.addEventListener('mouseover', function() {
+    column.style.backgroundColor = getRandomColor();
+  });
   for(let j=0;j<pixels;j++){
 
-    const div = document.createElement('div');
-    div.textContent = '';
-    div.style.backgroundColor = getRandomColor();
-    body.appendChild(div);
-    div.addEventListener('mouseover', function() {
-      div.style.backgroundColor = getRandomColor();
+    const row = document.createElement('row');
+    row.textContent = '';
+    row.style.backgroundColor = getRandomColor();
+    column.appendChild(row);
+    row.addEventListener('mouseover', function() {
+      row.style.backgroundColor = getRandomColor();
     });
     
   }
